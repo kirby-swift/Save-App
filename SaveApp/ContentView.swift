034309577903892eda5a0abdfcde2dc4d4eafaf3
Milda <Minecraft.kriper2284247@gmail.com>
2026-02-25 20:46:20 +0300

    Initial Commit

---
+++ SaveApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SaveApp
+//
+//  Created by Павлов Николай on 25.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SaveApp/SaveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveAppApp.swift
+//  SaveApp
+//
+//  Created by Павлов Николай on 25.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SaveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
